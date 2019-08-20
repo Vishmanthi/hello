844c61c6d66d11cbe375468d9b16a43646e43e5a
Vishmanthi <fernandovishmanthi@yahoo.com>
2019-08-20 16:01:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+const app = express();
+
+app.listen(8090, () => {
+    console.log('Server started!');
+  })
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { UpdateComponent } from './update/update.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UpdateComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
